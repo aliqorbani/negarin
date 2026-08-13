a2dd6a66a416248365ff8c418f88d4a7bca384b7
php3 <php3@noavarpub.com>
2026-08-13 17:52:23 +0330

    add red color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
         negarin: {
           ink: '#000000',   // near-black used for headings, header text, dark sections
           gray: '#333333',
+          red: '#AA2B2B',
           cream: '#F4EFE9', // warm off-white background used behind text blocks
           gold: '#B08D57',  // accent used for eyebrow labels / hover states
           line: '#E5E0D8',  // hairline borders
