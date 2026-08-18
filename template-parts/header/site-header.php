7e4b3f28f22fc005b1cc9a995b33f491966c4dce
php3 <php3@noavarpub.com>
2026-08-19 02:00:33 +0330

    some changes in header (max-w for custom logo)

--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -12,8 +12,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
-<header class="negarin-header sticky top-0 z-40 bg-white border-b border-black/5" x-data="{ menuOpen: false, searchOpen: false }">
-	<div class="max-w-7xl mx-auto px-4 py-4 flex flex-row-reverse items-center justify-between">
+<header class="negarin-header sticky top-0 z-40 bg-white border-b border-negarin-line" x-data="{ menuOpen: false, searchOpen: false }">
+	<div class="container max-w-7xl mx-auto px-4 py-4 flex flex-row-reverse items-center justify-between">
 
         <?php if ( has_custom_logo() ) : ?>
 
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
             <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="negarin-logo">
                 <img
                         src="<?php echo esc_url( $logo_url ); ?>"
-                        class="logo-image"
+                        class="logo-image max-w-[90px] md:max-w-[240px]"
                         alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>"
                 >
             </a>
@@ -54,9 +54,12 @@ if ( ! defined( 'ABSPATH' ) ) {
                 <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M13.6978 5.5C13.6978 6.56087 13.2764 7.57828 12.5263 8.32843C11.7761 9.07857 10.7587 9.5 9.69783 9.5C8.63696 9.5 7.61955 9.07857 6.8694 8.32843C6.11926 7.57828 5.69783 6.56087 5.69783 5.5M1.33105 4.90138L0.631049 13.3014C0.480672 15.1059 0.405483 16.0082 0.710515 16.7042C0.978516 17.3157 1.44286 17.8204 2.03002 18.1382C2.6983 18.5 3.60369 18.5 5.41447 18.5H13.9812C15.792 18.5 16.6974 18.5 17.3656 18.1382C17.9528 17.8204 18.4171 17.3157 18.6851 16.7042C18.9902 16.0082 18.915 15.1059 18.7646 13.3014L18.0646 4.90138C17.9352 3.34875 17.8705 2.57243 17.5267 1.98486C17.2239 1.46744 16.7731 1.0526 16.2323 0.793846C15.6182 0.500001 14.8392 0.500001 13.2812 0.500001L6.11447 0.5C4.55645 0.5 3.77745 0.5 3.16335 0.793844C2.62257 1.0526 2.17173 1.46744 1.86896 1.98486C1.52513 2.57243 1.46043 3.34875 1.33105 4.90138Z" stroke="#333333" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
-                <span class="negarin-cart-count absolute -top-2 -left-2 text-[10px] bg-negarin-ink text-white rounded-full w-4 h-4 flex items-center justify-center">
-					<?php get_template_part( 'template-parts/components/cart-drawer-count' ); ?>
-				</span>
+                	<?php $cart_count = WC()->cart->get_cart_contents_count();
+                    if($cart_count > 0){
+                        echo '<span class="negarin-cart-count absolute -top-2 -left-2 text-[10px] bg-negarin-ink text-white rounded-full w-4 h-4 flex items-center justify-center">';
+                        get_template_part( 'template-parts/components/cart-drawer-count' );
+                        echo '</span>';
+                    } ?>
 			</a>
 			<button @click="menuOpen = true" aria-label="<?php esc_attr_e( 'باز کردن منو', 'negarin' ); ?>">
                 <svg width="19" height="13" viewBox="0 0 19 13" fill="none" xmlns="http://www.w3.org/2000/svg">
